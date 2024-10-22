f34ba584dd88a98377aa333c49e8e8963d2286bb
ignosis-analytics <121535591+ignosis-analytics@users.noreply.github.com>
2024-10-22 19:03:14 +0000

    update test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+from elo import rate_1vs1
+
+result = rate_1vs1(800, 1200)
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building WooHoo Hee! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write(result)
